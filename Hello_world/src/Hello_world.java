281b4cd374fb11f9765a4562d61cf247c4ed9494
Carsonica <18eschenc56@millshighsch.org>
2017-08-18 09:52:35 -0700

    MyFirstProgram

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,15 @@
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+public class Hello_world 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From <your name>"); 
+//Display the string
+    }
+}
