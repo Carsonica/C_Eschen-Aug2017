e0db49300bf3a67b22dcab0cb74094bffffeb3ee
Carsonica <18eschenc56@millshighsch.org>
2017-08-21 09:12:48 -0700

    Second

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -1,15 +1,15 @@
 /**
  * Write a description of class Hello_world here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Carson Eschen (your name) 
+ * v1 (a version number or a date)
  */
 
 public class Hello_world 
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World! From <your name>"); 
+        System.out.println("Hello World! From Carson"); 
 //Display the string
     }
 }
