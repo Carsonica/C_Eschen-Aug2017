c1e96990af083b2cf04996bc91c5d1683cbdb1e3
Mark <Mark@Mark-Cyber-2015>
2017-08-30 00:10:07 -0700

    first home edit

--- Hello_world/src/Hello_world.java
+++ Hello_world/src/Hello_world.java
@@ -9,7 +9,7 @@ public class Hello_world
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World! From Carson"); 
+        System.out.println("Edit from home computer is a success."); 
 //Display the string
     }
 }
