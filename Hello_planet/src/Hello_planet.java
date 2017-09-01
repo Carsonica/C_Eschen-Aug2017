06ce6967e3514d1d42b0bb9966de9252bbbbd953
Carsonica <18eschenc56@millshighsch.org>
2017-09-01 09:32:00 -0700

    Nothing

--- Hello_planet/src/Hello_planet.java
+++ Hello_planet/src/Hello_planet.java
@@ -2,8 +2,12 @@
 public class Hello_planet {
 
 	public static void main(String[] args) {
-		System.out.print("Eclipse is really ");
-		System.out.println("difficult.");
+		//Pikachu!
+		
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("   (\\__/)");
+		System.out.println("   (o^.^)");
+		System.out.println("z(_(\")(\")");
 
 	}
 
--- WhatAboutThis/src/WhatAboutThis.java
+++ WhatAboutThis/src/WhatAboutThis.java
@@ -3,7 +3,6 @@ public class WhatAboutThis {
 
 	public static void main(String[] args) {
 		System.out.println("What about this?");
-
 	}
 
 }
