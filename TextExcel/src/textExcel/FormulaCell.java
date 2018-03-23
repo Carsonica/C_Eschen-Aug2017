4a81ace3fc36ee9b3362c3f765057b2d4684fea2
Carsonica <18eschenc56@millshighsch.org>
2018-03-23 09:52:17 -0700

    S

--- TextExcel/src/textExcel/FormulaCell.java
+++ TextExcel/src/textExcel/FormulaCell.java
@@ -1,4 +1,5 @@
 package textExcel;
+import java.util.ArrayList;
 
 public class FormulaCell extends RealCell {
 	public FormulaCell (String equation) {
@@ -6,10 +7,48 @@ public class FormulaCell extends RealCell {
 	}
 	
 	public String abbreviatedCellText() {
-		return "IAmFormula";
-	}
+		//Return the first 10 characters of the decimal, adding spaces as needed to fill the cell
+			String returnValue = "" + getDoubleValue();
+			if(("" + getDoubleValue()).length() > 9) { 
+				returnValue = ("" + getDoubleValue()).substring(0, 10);
+			}
+			while(returnValue.length() < 10) {
+				returnValue += " ";
+			}
+			return returnValue;
+		}
 	
 	public double getDoubleValue() {
+		//Split the equation at the spaces
+		String[] splitEquation = fullCellText().trim().split(" ");
+		
+		//Search the equation for operators, storing their indices in an array in order of operations
+		ArrayList<Integer> operatorIndices = new ArrayList<Integer>();
+		//Parse for SUM and AVG first, from left to right (to be implemented later)
+		//Parse for multiplication and division first, from left to right
+		for(int i = 1; i < splitEquation.length; i ++) {
+			if(splitEquation[i].equals("*") || splitEquation[i].equals("*")) {
+				operatorIndices.add(i);
+			}
+		}
+		//Parse for addition and subtraction, from left to right
+		for(int i = 1; i < splitEquation.length; i ++) {
+			if(splitEquation[i].equals("+") || splitEquation[i].equals("-")) {
+				operatorIndices.add(i);
+			}
+		}
+		for(int index: operatorIndices) {
+			if(splitEquation[index].equals("+")) {
+				fracAddition(splitEquation[index - 1], splitEquation[index + 1]);
+			} else if(splitEquation[index].equals("-")) {
+				//Make the second operator negative before calling addition
+				fracAddition(splitEquation[index - 1], "-" + splitEquation[index + 1]);
+			} else if(splitEquation[index].equals("*")) {
+				fracMultiplication(firstOperand, secondOperand);
+			} else(splitEquation[index].equals("/")) {
+				secondOperand.setReciprocal();
+				fracMultiplication(firstOperand, secondOperand);
+    	}
 		return 3.14;
 	}
 }
